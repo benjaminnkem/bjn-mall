cd1d5fde9c76cfc9dfbe12afd5256f82a74cc817
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-06-17 01:08:45 +0100

    Fixed sidebar problem, and animated the solution

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import "./dashboard.css";
 import "remixicon/fonts/remixicon.css";
 import Link from "next/link";
+import SideBarLinks from "../sidebarLinks";
 
 export const metadata = {
   title: "Dashboard",
@@ -11,67 +12,34 @@ export const metadata = {
 export default function DashboardLayout({ children }) {
   return (
     <>
-      <div id="dash-body" className="grid h-full">
-        <div className="h-full">
-          <aside className="overflow-x-hidden bg-stone-900 dark:bg-stone-800 text-stone-50 h-screen relative">
-            <div className="px-6">
-              <div className="flex items-center justify-between py-3">
-                <h1 className="text-2xl hidden sm:inline-block">BjnMall</h1>
-                <i className="ri-menu-2-line text-2xl hover:cursor-pointer"></i>
+      <div className="grid h-full">
+        <aside className="overflow-x-hidden bg-stone-900 dark:bg-stone-800 text-stone-50 h-full fixed lg:w-[320px] md:w-[280px] w-[80px] duration-200">
+          <div className="px-6">
+            <div className="flex items-center justify-between py-3">
+              <h1 className="text-2xl hidden sm:inline-block">BjnMall</h1>
+              <i className="ri-menu-2-line text-2xl hover:cursor-pointer"></i>
+            </div>
+            <div className="text-center mt-3">
+              <div className="overflow-hidden">
+                <Image
+                  src={`/images/admin1.jpg`}
+                  alt="Admin Pic"
+                  width={100}
+                  height={100}
+                  className="rounded-full mx-auto"
+                  draggable="false"
+                ></Image>
               </div>
-              <div className="text-center mt-3">
-                <div className="overflow-hidden">
-                  <Image
-                    src={`/images/admin1.jpg`}
-                    alt="Admin Pic"
-                    width={100}
-                    height={100}
-                    className="rounded-full mx-auto"
-                    draggable="false"
-                  ></Image>
-                </div>
-                <p className="text-stone-200 text-xs font-semibold mt-3">Admin Name</p>
-              </div>
+              <p className="text-stone-200 text-xs font-semibold mt-3">Admin Name</p>
             </div>
-            <div className="mt-5">
-              <Link href="/dashboard">
-                <div className="grid grid-cols-3 hover:shadow-md items-center px-6 duration-200 hover:bg-stone-800 py-3 select-none border-l-2 border-stone-50">
-                  <i className="ri-home-3-fill text-2xl"></i>
-                  <p className="hidden sm:inline-block">Dashboard</p>
-                </div>
-              </Link>
-              <Link href="/dashboard/products">
-                <div className="grid grid-cols-3 hover:shadow-md items-center px-6 duration-200 hover:bg-stone-800 py-3 select-none border-stone-50">
-                  <i className="ri-shopping-cart-fill text-2xl"></i>
-                  <p className="hidden sm:inline-block">Products</p>
-                </div>
-              </Link>
-              <Link href="/dashboard/users">
-                <div className="grid grid-cols-3 hover:shadow-md items-center px-6 duration-200 hover:bg-stone-800 py-3 select-none border-stone-50">
-                  <i className="ri-user-3-fill text-2xl"></i>
-                  <p className="hidden sm:inline-block">Users</p>
-                </div>
-              </Link>
-              <Link href="/dashboard/settings">
-                <div className="grid grid-cols-3 hover:shadow-md items-center px-6 duration-200 hover:bg-stone-800 py-3 select-none border-stone-50">
-                  <i className="ri-settings-3-fill text-2xl"></i>
-                  <p className="hidden sm:inline-block">Account</p>
-                </div>
-              </Link>
-              <Link href="#">
-                <div className="grid grid-cols-3 hover:shadow-md items-center px-6 duration-200 hover:bg-stone-800 py-3 select-none border-stone-50">
-                  <i className="ri-information-fill text-2xl"></i>
-                  <p className="hidden sm:inline-block">About</p>
-                </div>
-              </Link>
-            </div>
-            <div className="sticky bottom-2 mt-10 text-center">
-              <p className="text-xs text-stone-500 bottom-3 left-1/2 right-1/2">Created by Benjamin Nkem</p>
-            </div>
-          </aside>
-        </div>
+          </div>
+          <SideBarLinks />
+          <div className="sticky bottom-2 mt-10 text-center">
+            <p className="text-xs text-stone-500 bottom-3 left-1/2 right-1/2">Created by Benjamin Nkem</p>
+          </div>
+        </aside>
 
-        {children}
+        <div className="dark:bg-stone-950 lg:ml-[320px] md:ml-[280px] ml-[80px] duration-200">{children}</div>
       </div>
     </>
   );
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,85 +2,82 @@ export default function DashboardHome() {
   const username = "Benjamin";
   return (
     <>
-      <div className="dark:bg-stone-950">
-        <div className="py-20 px-10 bg-stone-200 dark:bg-stone-900">
-          <h2 className="font-semibold">Welcome Back, {username} 😎</h2>
-          <p className="text-sm">
-            Last Seen: <span className="font-semibold text-stone-700 dark:text-stone-200">{new Date().toLocaleTimeString()}</span>
-          </p>
-        </div>
+      <div className="py-20 px-10 bg-stone-200 dark:bg-stone-900">
+        <h2 className="font-semibold">Welcome Back, {username} 😎</h2>
+        <p className="text-sm">
+          Last Seen:{" "}
+          <span className="font-semibold text-stone-700 dark:text-stone-200">{new Date().toLocaleTimeString()}</span>
+        </p>
+      </div>
 
-        <div className="main-dash-side mt-10 md:px-10 sm:px-6 px-4">
-          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2 -mt-5 px-5">
-            <div className="bg-green-600 rounded-md shadow-md hover:shadow-2xl text-green-50 px-8 py-6 duration-200 hover:bg-green-500 hover:cursor-pointer select-none">
-              <div className="flex flex-col justify-between h-full space-y-4">
-                <h3 className="font-semibold">Customers</h3>
-                <div className="flex justify-between items-center text-2xl">
-                  <i className="ri-user-5-fill"></i>
-                  <p>5</p>
-                </div>
-              </div>
-            </div>
-            <div className="bg-purple-600 rounded-md shadow-md hover:shadow-2xl text-green-50 px-8 py-6 duration-200 hover:bg-purple-500 hover:cursor-pointer select-none">
-              <div className="flex flex-col justify-between h-full space-y-4">
-                <h3 className="font-semibold">Orders</h3>
-                <div className="flex justify-between items-center text-2xl">
-                  <i className="ri-shopping-cart-fill"></i>
-                  <p>114</p>
-                </div>
-              </div>
-            </div>
-            <div className="bg-red-600 rounded-md shadow-md hover:shadow-2xl text-green-50 px-8 py-6 duration-200 hover:bg-red-500 hover:cursor-pointer select-none">
-              <div className="flex flex-col justify-between h-full space-y-4">
-                <h3 className="font-semibold">Users</h3>
-                <div className="flex justify-between items-center text-2xl">
-                  <i className="ri-user-2-line"></i>
-                  <p>1.2k</p>
-                </div>
+      <div className="main-dash-side mt-10 md:px-10 sm:px-6 px-4">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-2 -mt-5 px-5">
+          <div className="bg-green-600 rounded-md shadow-md hover:shadow-2xl text-green-50 px-8 py-6 duration-200 hover:bg-green-500 hover:cursor-pointer select-none">
+            <div className="flex flex-col justify-between h-full space-y-4">
+              <h3 className="font-semibold">Customers</h3>
+              <div className="flex justify-between items-center text-2xl">
+                <i className="ri-user-5-fill"></i>
+                <p>5</p>
               </div>
             </div>
           </div>
-
-          <div className="grid md:grid-cols-2 gap-8 mt-10 px-5">
-            <div className="p-5 shadow-md max-w-5xl bg-transparent dark:bg-stone-800 rounded-md">
-              <h3 className="text-stone-900 dark:text-stone-200 font-bold text-lg">Recent Activities</h3>
-              <div className="mt-3 space-y-2">
-                <div className="flex items-center space-x-4">
-                  <i className="ri-check-fill font-bold text-green-500 text-lg"></i>
-                  <p>
-                    Successfully delivered the product to{" "}
-                    <span className="text-green-500 font-semibold border-b border-green-500 cursor-pointer">
-                      Davicci
-                    </span>
-                  </p>
-                </div>
-                <div className="flex items-center space-x-4">
-                  <i className="ri-truck-line font-bold text-yellow-500 text-lg"></i>
-                  <p>
-                    Delivery to{" "}
-                    <span className="text-yellow-500 font-semibold border-b border-yellow-500 cursor-pointer">
-                      Potler
-                    </span>{" "}
-                    still Pending
-                  </p>
-                </div>
-                <div className="flex items-center space-x-4">
-                  <i className="ri-close-line font-bold text-red-500 text-lg"></i>
-                  <p>
-                    <span className="text-red-500 font-semibold border-b border-red-500 cursor-pointer">Michael</span>{" "}
-                    Cancelled his/her order before delivery
-                  </p>
-                </div>
+          <div className="bg-purple-600 rounded-md shadow-md hover:shadow-2xl text-green-50 px-8 py-6 duration-200 hover:bg-purple-500 hover:cursor-pointer select-none">
+            <div className="flex flex-col justify-between h-full space-y-4">
+              <h3 className="font-semibold">Orders</h3>
+              <div className="flex justify-between items-center text-2xl">
+                <i className="ri-shopping-cart-fill"></i>
+                <p>114</p>
               </div>
             </div>
-            <div className="p-10 shadow-md max-w-5xl bg-transparent dark:bg-stone-800 rounded-md grid place-content-center text-center">
-              <h3 className="text-xl font-semibold">Stock Statistics</h3>
-              <p>Coming Soon.</p>
+          </div>
+          <div className="bg-red-600 rounded-md shadow-md hover:shadow-2xl text-green-50 px-8 py-6 duration-200 hover:bg-red-500 hover:cursor-pointer select-none">
+            <div className="flex flex-col justify-between h-full space-y-4">
+              <h3 className="font-semibold">Users</h3>
+              <div className="flex justify-between items-center text-2xl">
+                <i className="ri-user-2-line"></i>
+                <p>1.2k</p>
+              </div>
             </div>
           </div>
-
-          <div className="h-20"></div>
         </div>
+
+        <div className="grid md:grid-cols-2 gap-8 mt-10 px-5">
+          <div className="p-5 shadow-md max-w-5xl bg-transparent dark:bg-stone-800 rounded-md">
+            <h3 className="text-stone-900 dark:text-stone-200 font-bold text-lg">Recent Activities</h3>
+            <div className="mt-3 space-y-2">
+              <div className="flex items-center space-x-4">
+                <i className="ri-check-fill font-bold text-green-500 text-lg"></i>
+                <p>
+                  Successfully delivered the product to{" "}
+                  <span className="text-green-500 font-semibold border-b border-green-500 cursor-pointer">Davicci</span>
+                </p>
+              </div>
+              <div className="flex items-center space-x-4">
+                <i className="ri-truck-line font-bold text-yellow-500 text-lg"></i>
+                <p>
+                  Delivery to{" "}
+                  <span className="text-yellow-500 font-semibold border-b border-yellow-500 cursor-pointer">
+                    Potler
+                  </span>{" "}
+                  still Pending
+                </p>
+              </div>
+              <div className="flex items-center space-x-4">
+                <i className="ri-close-line font-bold text-red-500 text-lg"></i>
+                <p>
+                  <span className="text-red-500 font-semibold border-b border-red-500 cursor-pointer">Michael</span>{" "}
+                  Cancelled his/her order before delivery
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="p-10 shadow-md max-w-5xl bg-transparent dark:bg-stone-800 rounded-md grid place-content-center text-center">
+            <h3 className="text-xl font-semibold">Stock Statistics</h3>
+            <p>Coming Soon.</p>
+          </div>
+        </div>
+
+        <div className="h-20"></div>
       </div>
     </>
   );

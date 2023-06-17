97bb90da7ee3201b6edf239821373b1bce4b38f2
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-06-17 02:20:27 +0100

    Soon get usePathname

---
+++ app/dashboard/PathNameCheck.js
@@ -0,0 +1,13 @@
+"use client";
+import { usePathname } from "next/navigation";
+
+export default function SideBarLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      <div>
+        ss
+      </div>
+    </>
+  );
+}
